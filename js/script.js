4b0b97efd22effa8ccf68e6661def30412b539e0
Huseynitsu <huseynquliyev395@gmail.com>
2024-12-29 11:47:20 +0400

    changes3

--- js/script.js
+++ js/script.js
@@ -15,6 +15,14 @@ window
   .addEventListener("change", setFavicon);
 
 $(document).ready(function () {
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 80) {
+      $("header").addClass("sticky");
+    } else {
+      $("header").removeClass("sticky");
+    }
+  });
+
   // hamburger menu
   $(".menu-wrap").click(function (e) {
     $("header .menu-wrapper").animate({ top: "0" }, 300, "linear");
@@ -32,13 +40,6 @@ $(document).ready(function () {
       $(".navbar-collapse").removeClass("show");
     }, 300);
   });
-
-  // // body click
-  // $("body").click(function () {
-  //   $("body").css("overflow-y", "visible");
-  //   $(".lang-list").slideUp();
-  //   $("header .menu-wrapper").animate({ top: "-100%" }, 300, "linear");
-  // });
 
   var swiper = new Swiper(".swiper-container-h", {
     direction: "horizontal",
@@ -96,6 +97,62 @@ $(document).ready(function () {
       el: ".swiper-pagination",
       type: "bullets",
       clickable: "true",
+    },
+  });
+
+  var swiper = new Swiper(".swiper-container-h1", {
+    direction: "horizontal",
+    effect: "slide",
+    autoplay: false,
+    parallax: true,
+    speed: 1600,
+    rtl: true,
+    loop: true,
+    loopFillGroupWithBlank: !0,
+    keyboard: {
+      enabled: true,
+      onlyInViewport: true,
+    },
+    scrollbar: {
+      el: ".swiper-scrollbar",
+      hide: false,
+      draggable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next2",
+      prevEl: ".swiper-button-prev2",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      type: "bullets",
+      clickable: "true",
+    },
+  });
+
+  var swiperProduct = new Swiper(".productSwiper", {
+    direction: "horizontal",
+    effect: "slide",
+    autoplay: false,
+    parallax: true,
+    speed: 1600,
+    rtl: true,
+    loop: true,
+    centeredSlides: true,
+    loopFillGroupWithBlank: true,
+    slidesPerView: 5,
+    spaceBetween: 20,
+    keyboard: {
+      enabled: true,
+      onlyInViewport: true,
+    },
+    scrollbar: {
+      el: ".swiper-scrollbar",
+      hide: false,
+      draggable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next2",
+      prevEl: ".swiper-button-prev2",
     },
   });
 });
